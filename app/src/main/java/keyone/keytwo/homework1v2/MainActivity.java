5b19cf29d5cc34e60494c2976d03db3162729270
MEA <mayer_evgenija@mail.ru>
2021-08-13 19:34:04 +0700

    Добавила listener и Intent, чтобы из этого приложения открывался калькулятор

--- app/src/main/java/keyone/keytwo/homework1v2/MainActivity.java
+++ app/src/main/java/keyone/keytwo/homework1v2/MainActivity.java
@@ -2,15 +2,24 @@ package keyone.keytwo.homework1v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main_second);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main_second);
 
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent("runCalculator");
+                startActivity(i);
+            }
+        });
     }
 }
